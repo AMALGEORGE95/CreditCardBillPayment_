acdd37d46b85a097eac000073ff7b260c9cc7562
Amal George <amal.george@sayonetech.com>
2023-01-31 13:06:12 +0530

    userFunctionalities

--- src/main/java/com/creditcard/Creditcard/repository/AddressRepository.java
+++ src/main/java/com/creditcard/Creditcard/repository/AddressRepository.java
@@ -1,9 +1,12 @@
 package com.creditcard.Creditcard.repository;
 
 import com.creditcard.Creditcard.entity.AddressEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends CrudRepository<AddressEntity,Long> {
+    @Query(value ="SELECT * FROM address a WHERE a.id=?1 and a.user_id=?2 ",nativeQuery = true)
+    AddressEntity findByIdAndUserId(Long addressId, Long id);
 }
